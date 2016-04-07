e58758be4526b5857b3cabb7ee9ee1ac1846dc35
Victor Ribeiro <victormech@gmail.com>
2016-04-07 01:04:08 -0300

    Adding basic classes skeleton

---
+++ src/TextFileValidator.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created with PHP 5.6 generator
+ * User: Victor Ribeiro <victormech@gmail.com>
+ * PHP 5.6 generator created by Victor MECH - April 2016
+*/
+
+namespace LazyEight\DiTesto;
+
+
+use LazyEight\DiTesto\ValueObject\FileLocation;
+
+class TextFileValidator
+{
+
+    /*
+     * @var FileLocation
+     */
+    private $location;
+    
+    /**
+     * @param FileLocation $location
+     */
+    public function __construct(FileLocation $location)
+    {
+        // TODO: Implement __construct method.
+    }
+	
+    /**
+     * @return bool
+     */
+    public function validate()
+    {
+        // TODO: Implement validate method.
+    }
+	
+    /**
+     * @return FileLocation
+     */
+    public function getFileLocation()
+    {
+        // TODO: Implement getFileLocation method.
+    }
+	
+}
