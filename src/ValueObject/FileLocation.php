71fc0feb33f7615c9714b7f5f29654bbb6d051d9
Victor Ribeiro <victormech@gmail.com>
2016-04-11 00:28:12 -0300

    [ADD] method getFileDirectory on FileLocation class

--- src/ValueObject/FileLocation.php
+++ src/ValueObject/FileLocation.php
@@ -13,7 +13,7 @@ use LazyEight\BasicTypes\Stringy;
 
 class FileLocation implements ValueObjectInterface
 {
-    /*
+    /**
      * @var Stringy
      */
     private $location;
@@ -32,5 +32,13 @@ class FileLocation implements ValueObjectInterface
     public function getValue()
     {
         return clone $this->location;
+    }
+
+    /**
+     * @return Stringy
+     */
+    public function getFileDirectory()
+    {
+        return $this->location->substring(0, $this->location->lastIndexOf("/"));
     }
 }
